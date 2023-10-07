fbd2efa7319c5706a237f5751dfa7a96bc9be502
Andrés Banda <andrezh_gln7@live.com>
2023-10-06 22:54:04 -0500

    Modify cards for benefits
    Add images instead of numbers in benefit cards.
    Separate home markup to a new file.

---
+++ src/home.ts
@@ -0,0 +1,44 @@
+export const homeMarkup = `
+<div>
+  <section class="header__container">
+    <div class="header__content">
+      <h1 class="header__title">Create your own startup </h1>
+      <p class="header__text">Turn your idea into a thriving startup in just days, with guidance through every step from concept to funding and brand creation. Experience the thrill of entrepreneurship and see your vision become a reality.</p>
+      <div class="header__button-container">
+        <button class="header__button">Let's start</button>
+        <a href="#howit" class="header__button--outline">Learn more</a>
+      </div>
+    </div>
+    <div id="lottie" class="header__lottie"></div>
+  </section>
+
+  <section class="main__container" id="howit">
+    <div class="main__content">
+      <h2 class="main__title">How it works?</h2>
+      <p class="main__text">Launch is your essential partner in turning that spark of an idea into a market-ready product or service. No prior experience needed; all you need is a vision, and we provide the roadmap.</p>
+      <div class="main__benefits-container">
+        <div class="main__benefit">
+          <img class="main__benefit-img" src="/public/ideasPink.jpg"/>
+          <h3 class="main__benefit-title">Find Your Idea</h3>
+          <p class="main__benefit-description">Turn your thoughts and inspirations into a clear, viable idea. No matter how big or small, this is where it all starts.</p>
+        </div>
+        <div class="main__benefit">
+        <img class="main__benefit-img" src="/public/booksPink.jpg"/>
+        <h3 class="main__benefit-title">Build Your Vision</h3>
+          <p class="main__benefit-description">With tools and guidance at your fingertips, shape and model your idea. It's time to give it form and get it ready for the world.</p>
+        </div>
+        <div class="main__benefit">
+        <img class="main__benefit-img" src="/public/rocketPink.jpg"/>
+        <h3 class="main__benefit-title">Make It Real</h3>
+          <p class="main__benefit-description">With our support, take your idea to the market. Whether it's a product, a service, or a solution, we're here to help you launch it successfully.</p>
+        </div>
+      </div>      
+      </div>
+  </section>
+
+  <footer class="footer__container">
+    <div class="footer__content">
+      <p class="footer__text">Launch 2023</p>
+    </div>
+</div>
+`;
--- src/main.ts
+++ src/main.ts
@@ -2,52 +2,9 @@ import "./style.css";
 import { setupLottie } from "./lottie.ts";
 import { setupLogin } from "./login.ts";
 import { setupRoutes } from "./routes.ts";
+import { homeMarkup } from "./home.ts";
 
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <div>
-    <section class="header__container">
-      <div class="header__content">
-        <h1 class="header__title">Create your own startup </h1>
-        <p class="header__text">urn your idea into a thriving startup in just days, with guidance through every step from concept to funding and brand creation. Experience the thrill of entrepreneurship and see your vision become a reality.</p>
-        <div class="header__button-container">
-          <button class="header__button">Let's start</button>
-          <a href="#howit" class="header__button--outline">Learn more</a>
-        </div>
-      </div>
-      <div id="lottie" class="header__lottie"></div>
-    </section>
-
-    <section class="main__container" id="howit">
-      <div class="main__content">
-        <h2 class="main__title">How it works?</h2>
-        <p class="main__text">Startup creator 3000 is a tool that helps you to create your own startup in few days. You don't need to know how to code, you don't need to know how to design. You just need to have an idea and we will do the rest.</p>
-        <div class="main__benefits-container">
-          <div class="main__benefit">
-            <p class="main__benefit-number">1</p>
-            <h3 class="main__benefit-title">The mission</h3>
-            <p>Define your mission and vision</p>
-          </div>
-          <div class="main__benefit">
-            <p class="main__benefit-number">2</p>
-            <h3 class="main__benefit-title">The value proposition</h3>
-            <p>Define your value proposition</p>
-          </div>
-          <div class="main__benefit">
-            <p class="main__benefit-number">3</p>
-            <h3 class="main__benefit-title">The market research</h3>
-            <p>Do a market research</p>
-          </div>
-        </div>      
-        </div>
-    </section>
-
-    <footer class="footer__container">
-      <div class="footer__content">
-        <p class="footer__text">Launch 2023</p>
-      </div>
-  </div>
-`;
-
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = homeMarkup;
 setupRoutes(document.querySelector<HTMLDivElement>("#app")!);
 setupLottie(document.querySelector<HTMLDivElement>("#lottie")!);
 setupLogin(
