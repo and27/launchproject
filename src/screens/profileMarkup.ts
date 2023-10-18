9783d098f8b635081476bb1c76e6107d774f04d8
Andrés Banda <andrezh_gln7@live.com>
2023-10-18 01:08:17 -0500

    Embed video test

--- src/screens/profileMarkup.ts
+++ src/screens/profileMarkup.ts
@@ -4,4 +4,5 @@ export const profileMarkup = `
         <h1 class="profile__title">Mi perfil</h1>
         <p class="profile__text">Completa tu perfil para poder acceder a la plataforma</p>
     </div>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/CsRhoqK74RQ?si=0-L8QW6RhkX_6Pq8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
     </section>`;
