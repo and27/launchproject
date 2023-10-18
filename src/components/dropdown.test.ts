c38c1dff3dd47cd3ae6f5f586636f0b1cf22b971
Andrés Banda <andrezh_gln7@live.com>
2023-10-18 01:12:02 -0500

    Add dropdown test

--- src/components/dropdown.test.ts
+++ src/components/dropdown.test.ts
@@ -13,4 +13,32 @@ describe("Dropdown", () => {
   afterEach(() => {
     document.body.removeChild(container);
   });
+
+  it("should close the dropdown when clicking outside of the login button", async () => {
+    const dropdown = container.querySelector(".dropdown-content");
+    expect(dropdown?.classList.contains("show")).toBe(false);
+
+    const loginButton = container.querySelector(
+      "#loginButton"
+    ) as HTMLButtonElement;
+    loginButton?.click();
+    expect(dropdown?.classList.contains("show")).toBe(true);
+
+    document.body.click();
+    expect(dropdown?.classList.contains("show")).toBe(false);
+  });
+
+  it("should not close the dropdown when clicking on the login button", async () => {
+    const dropdown = container.querySelector(".dropdown-content");
+    expect(dropdown?.classList.contains("show")).toBe(false);
+
+    const loginButton = container.querySelector(
+      "#loginButton"
+    ) as HTMLButtonElement;
+    loginButton?.click();
+    expect(dropdown?.classList.contains("show")).toBe(true);
+
+    loginButton?.click();
+    expect(dropdown?.classList.contains("show")).toBe(true);
+  });
 });
